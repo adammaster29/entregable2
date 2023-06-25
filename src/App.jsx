ccafe991cdd46af8e7ef26a9e0763bc175d52ee0
adammaster29 <104658788+adammaster29@users.noreply.github.com>
2023-06-25 12:23:42 -0500

    file/buscador

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import axios from "axios";
 
@@ -7,7 +6,9 @@ function App() {
   const [weather, setWeather] = useState({});
   const [isCelsio, setIsCelsio] = useState(true);
   const [temperature, setTemperature] = useState(true);
-  const isclick = () => setIsCelsio(!isCelsio);
+  const [searchQuery, setSearchQuery] = useState(""); // Nuevo estado para el campo de búsqueda
+
+  const handleClick = () => setIsCelsio(!isCelsio);
 
   useEffect(() => {
     const success = (pos) => {
@@ -31,56 +32,60 @@ function App() {
   }, []);
 
   console.log(weather);
+
+  const buscar = () => {
+    // Actualizar el campo 'name' de 'weather' con el valor del campo de búsqueda
+    setWeather({ ...weather, name: searchQuery });
+  };
+
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
   return (
     <div className="App">
-
       <div className="time">
-   
-        
-     
+       <div className="input-contenedor">
+       <input
+          type="text"
+          placeholder="Buscar ciudad"
+          value={searchQuery}
+          onChange={handleChange}
+        />
+        <i onClick={buscar} className="bx bx-search lupa"></i>
 
-
-        <input type="text" placeholder="Buscar ciudad" />
+       </div>
         <h1 className="title--weather">weather app</h1>
-        
-        <button onClick={isclick} className="btn">
-          {" "}
-          {isCelsio ? " °F" : " °C"}{" "}
+        <button onClick={handleClick} className="btn">
+          {isCelsio ? " °F" : " °C"}
         </button>
       </div>
 
-
-      {/* nubes */}
       <div className="cloud">
-
         <div className="nubes">
           <p>
             {weather.name} &nbsp; <span>{weather.sys?.country}</span>
           </p>
           <img
             className="img--clouds"
-            src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png`}
+            src={`http://openweathermap.org/img/wn/${
+              weather.weather?.[0].icon
+            }@2x.png`}
             alt=""
           />
           <p>{weather.weather?.[0].description}</p>
         </div>
-
-        <i style={{color:"white"}} className='bx bx-cloud-lightning bx-fade-down bx-lg' ></i>
+        <i style={{ color: "white" }} className="bx bx-cloud-lightning bx-fade-down bx-lg"></i>
         <h1 className="temp">
-          {" "}
           <span style={{ color: "white" }}>
             {isCelsio ? temperature?.celsiu : temperature?.farenheit}
           </span>
         </h1>
-
       </div>
-      {/*  */}
 
       <div className="time">
-
-
         <p className="estados-clima">
-          <span className="name">Temp_min:</span> <span>{` ${weather.main?.temp_main} `}</span>
+          <span className="name">Temp_min:</span> <span>{` ${weather.main?.temp_min} `}</span>
         </p>
         <p className="estados-clima">
           <span className="name">Temp_max:</span> <span>{` ${weather.main?.temp_max} `}</span>
@@ -95,29 +100,9 @@ function App() {
           <span className="name">Humidity: </span> <span>{` ${weather.main?.humidity} %`}</span>
         </p>
         <p className="estados-clima">
-          <span className="name">Wind speed: </span>  <span> {` ${weather.wind?.speed} m/s`}</span>
+          <span className="name">Wind speed: </span> <span>{` ${weather.wind?.speed} m/s`}</span>
         </p>
-
-
-
-
-
-
-
       </div>
-
-
-
-
-      
-
-
-
-
-
-
-
-
     </div>
   );
 }
